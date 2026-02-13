a05a10ac8774cb98a7acbc63a26dff6029f0466a
Jefrexon <jefrexon@gmail.com>
2026-02-13 17:00:59 +0700

    Update Parser.js

--- Parser.js
+++ Parser.js
@@ -1,1 +1,11 @@
+(function () {
+  'use strict';
 
+  Lampa.Utils.putScriptAsync(
+    ['https://jefrexon-blip.github.io/lampa-plugins/pubtorr.js'],
+    function () {
+      console.log('[PubTorr] loaded');
+    }
+  );
+
+})();
