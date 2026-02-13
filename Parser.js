399ac9a3718f7a7a8bba42a0155e9ddf9918c319
Jefrexon <jefrexon@gmail.com>
2026-02-13 17:06:57 +0700

    Refactor script loading for PubTorr plugin

--- Parser.js
+++ Parser.js
@@ -1,11 +1,21 @@
 (function () {
   'use strict';
 
-  Lampa.Utils.putScriptAsync(
-    ['https://jefrexon-blip.github.io/lampa-plugins/pubtorr.js'],
-    function () {
-      console.log('[PubTorr] loaded');
-    }
-  );
+  var URL = 'https://jefrexon-blip.github.io/lampa-plugins/pubtorr.js';
+  var VERSION = '1.0.0-prod';
 
+  function boot() {
+    if (!window.Lampa || !Lampa.Utils || !Lampa.Utils.putScriptAsync) {
+      return setTimeout(boot, 250);
+    }
+
+    if (window.__pubtorr_loader_loaded) return;
+    window.__pubtorr_loader_loaded = true;
+
+    Lampa.Utils.putScriptAsync([URL + '?v=' + encodeURIComponent(VERSION)], function () {
+      try { console.log('[PubTorr] loaded', VERSION); } catch (e) {}
+    });
+  }
+
+  boot();
 })();
